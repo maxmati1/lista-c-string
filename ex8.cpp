895e6f30583f6b1f2a55487d41537bbbd03124d0
max <mizepefacul@outlook.com>
2024-10-17 14:04:57 -0300

    Update ex8.cpp

--- ex8.cpp
+++ ex8.cpp
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-	char palavra[50],palavra2[50];
-	int i,comp1,comp2,cont=0;
-	
-	printf("Digite uma pequena palavra");
-	scanf("%s",palavra);
-	printf("Digite a frase:");
-	scanf("%s",palavra2);
-	
-	comp1= strlen(palavra);
-	comp2 = strlen(palavra2);
-	for(i=0;i<=comp2;i++){
-		if(strcmp(palavra[i],palavra2[i])==0){
-			cont++;
-		}
-	}
-	printf("A menor palavra ocorre %d vezes",cont);
-	
-	return 0;
+int main() {
+    char palavra[50], frase[100];
+    int i, j, k, comp1, comp2, cont = 0, tem;
+
+    printf("Digite uma pequena palavra: ");
+    scanf("%s", palavra);
+
+    printf("Digite a frase: ");
+    scanf(" %[^\n]", frase);  
+
+    comp1 = strlen(palavra);
+    comp2 = strlen(frase);
+
+    for (i = 0; i <= comp2 - comp1; i++) {
+        tem = 1;
+        for (j = 0, k = i; j < comp1; j++, k++) {
+            if (frase[k] != palavra[j]) {
+                tem = 0;
+                break;
+            }
+        }
+        if (tem) {
+            cont++;
+        }
+    }
+
+    printf("A menor palavra ocorre %d vezes\n", cont);
+
+    return 0;
 }
